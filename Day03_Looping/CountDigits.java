28f34da7651df650b29b7085122de00d73924678
PrachiSuryawanshi31 <suryawanshiprachi35@gmail.com>
2025-12-28 21:16:05 +0530

    Added Program to Count Digits in a Number using while Loop .

---
+++ Day03_Looping/CountDigits.java
@@ -0,0 +1,23 @@
+//Create a program to Count Digits in a Number number by using while.
+
+import java.util.Scanner;
+
+class CountDigits
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number:");
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		while(num > 0)
+		{
+			count ++;
+			num = num / 10;
+		}
+
+		System.out.println("Total digits :"+count);
+	}
+}
