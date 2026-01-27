50582c34cd8a1aff7048aad5ae6aacd049e329d9
PrachiSuryawanshi31 <suryawanshiprachi35@gmail.com>
2026-01-27 23:21:25 +0530

    Implemented simple method overriding to demonstrate runtime polymorphism.

---
+++ Day12_Polymorphism/02_Method_Overriding/Dog.java
@@ -0,0 +1,20 @@
+
+class Animal{
+	void sound()
+	{
+		System.out.println("Animal males Sound");
+	}
+}
+
+class Dog extends Animal
+{
+	void sound(){
+		System.out.println("Dog Barks");
+	}
+
+	public static void main(String[] args) {
+		
+		Animal a =new Dog();
+		a.sound();
+	}
+}
