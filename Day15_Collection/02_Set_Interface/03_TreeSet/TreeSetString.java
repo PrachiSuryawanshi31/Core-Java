0759c65ec124c19d4c98abe53e12e7a1b93a1239
PrachiSuryawanshi31 <suryawanshiprachi35@gmail.com>
2026-02-11 21:58:07 +0530

    Implement TreeSet example for alphabetical string sorting.

---
+++ Day15_Collection/02_Set_Interface/03_TreeSet/TreeSetString.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetString {
+    public static void main(String[] args) {
+
+        Set<String> names = new TreeSet<>();
+
+        names.add("Siya");
+        names.add("Riya");
+        names.add("Kiya");
+        names.add("Anaya");
+
+        System.out.println(names);
+    }
+}
